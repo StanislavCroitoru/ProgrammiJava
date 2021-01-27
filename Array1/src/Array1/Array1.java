9ec042e45871fcca2ae2ed3029456140d4614d66
Stanislav Croitoru <stanislav.croitoru@polomanettiporciatti.edu.it>
2021-01-27 15:51:57 +0100

    consegna1

---
+++ Array1/src/Array1/Array1.java
@@ -0,0 +1,8 @@
+package Array1;
+
+public class Array1
+{
+    private String[] nomeatleta;
+     private String[] squadra;
+      private int [] golfatti;
+}
---
+++ Array1/src/Array1/Main.java
@@ -0,0 +1,13 @@
+package Array1;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+	// write your code here
+
+    }
+
+}
