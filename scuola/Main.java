cf4e9b22737ec21535e3dba15728382808b278bd
Stanislav Croitoru <stanislav.croitoru@polomanettiporciatti.edu.it>
2021-02-21 17:03:23 +0100

    consegna

---
+++ scuola/Main.java
@@ -0,0 +1,35 @@
+package scuola;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        byte arrayLenght = 0;
+         int i = 0;
+
+        Scanner reqInput = new Scanner(System.in);
+         scuola scuola = new scuola();
+          Random rand = new Random();
+
+        System.out.println("Quanti studenti ci sono nella classe................................................? > ");
+         arrayLenght = reqInput.nextByte();
+
+         Class scuolaArrayObj[]= new Class[arrayLenght];
+
+        int scuolaAdd;
+
+        for(i = 0; i < arrayLenght ; i++)
+        {
+            scuolaAdd =
+                    System.out.println(array[i]);
+            somma = somma + array[i];
+
+        }
+
+    }
+
+}
+
---
+++ scuola/scuola.java
@@ -0,0 +1,9 @@
+package scuola;
+
+public class scuola
+{
+    public String nome;
+     public String numerodeglistudenti;
+
+    public void addizionastudenti(String addizionastudenti_nome)
+}
