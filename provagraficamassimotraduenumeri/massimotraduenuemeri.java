047dc2dd143eced1094afaf8ce17ff2acaf26ae5
Stanislav Croitoru <stanislav.croitoru@polomanettiporciatti.edu.it>
2021-03-16 18:17:27 +0100

    consegna

---
+++ provagraficamassimotraduenumeri/massimotraduenuemeri.java
@@ -0,0 +1,13 @@
+package com.provagraficamassimotraduenumeri;
+
+import javax.swing.*;
+
+public class massimotraduenuemeri {
+    private JLabel lblprimonumero;
+    private JTextField txtinserimentoprimonumero;
+    private JLabel lblsecondonumero;
+    private JTextField txtinserimentosecondonumero;
+    private JPanel provamassimotradue;
+    private JLabel lblmassimonumero;
+    private JTextField txtvisualizzazionenumero;
+}
